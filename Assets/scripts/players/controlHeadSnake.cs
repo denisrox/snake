23dd9ee97295773b5a9bad5a3d49f9569cb14a40
Denis Shaporov <denisrox@mail.ru>
2020-06-08 00:36:15 +0300

    proverka

--- Assets/scripts/players/controlHeadSnake.cs
+++ Assets/scripts/players/controlHeadSnake.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class controlHeadSnake : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speedMove;
+    public float speedRotatet;
+    GameObject headSnake;
     void Start()
     {
-        
+        headSnake = gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //headSnake.GetComponent<GameObject>().transform.forward;
     }
 }
